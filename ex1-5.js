4bcee6b0294917a91f09f08ebd6d419334b9839d
yagiayaka <167832854+yagiayaka@users.noreply.github.com>
2025-01-14 21:31:33 +0900

    Update ex1-5.js

--- ex1-5.js
+++ ex1-5.js
@@ -1,2 +1,2 @@
-function nazo15(){if(document.getElementById('answer15').value!='韓国'){alert('はずれ!');}
+function nazo15(){if(document.getElementById('answer15').value!='2'){alert('はずれ!');}
                 else{alert('あたり！');}}
