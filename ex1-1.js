fbef6accca1167693c08f3e1831ac00f59f6c4bf
yagiayaka <167832854+yagiayaka@users.noreply.github.com>
2025-01-14 21:29:59 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,5 +1,5 @@
 function imgchange1() {
-    document.getElementById('d').src = './image/6.jpeg';
+    document.getElementById('d').src = './image/6.jpg';
 }
 function imgchange2() {
     document.getElementById('d').src = './image/5.jpg';
